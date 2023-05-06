1f58f34d8f4c8695585ab86cd70d7dc6cec877a0
AlexKaylinir <alexkaylinir@gmail.com>
2023-05-06 19:16:41 +0300

    new

--- src/main/java/com/example/homework_6_1/HomeController.java
+++ src/main/java/com/example/homework_6_1/HomeController.java
@@ -1,2 +1,17 @@
-package com.example.homework_6_1;public class HomeController {
+package com.example.homework_6_1;
+
+import ch.qos.logback.core.model.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("message")
+    public String message(Model model) {
+        model.addAttribute("message", "Привет, Thymeleaf!");
+        return "x";
+    }
 }
+
+
--- src/main/java/com/example/homework_6_1/HomeWork61Application.java
+++ src/main/java/com/example/homework_6_1/HomeWork61Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HomeWork61Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HomeWork61Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWork61Application.class, args);
+    }
 
 }
